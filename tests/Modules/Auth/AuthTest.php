76988cc2ddced2c41fa22e4121a9edfb3ef7316b
RafalZaleski <rafal.zaleski@wp.pl>
2024-07-02 17:55:31 +0200

    remove unnecessary test

--- tests/Modules/Auth/AuthTest.php
+++ tests/Modules/Auth/AuthTest.php
@@ -33,16 +33,6 @@ class AuthTest extends TestCase
             'password' => '12345',
         ];
     }
-    /**
-     * A basic test example.
-     */
-
-    public function test_the_application_returns_a_successful_response(): void
-    {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
-    }
 
     public function test_register(): User
     {
